92e2d960ddce5344761aa014eeb1d8398dab9f17
Premnathkulal <premnathkulal199@gmail.com>
2024-06-19 18:04:14 +0530

    basic list card implementation is done

--- src/Pages/home/Home.tsx
+++ src/Pages/home/Home.tsx
@@ -1,12 +1,68 @@
-import ListCard from '../../components/list-card/ListCard'
-import './Home.scss'
+import ListCard from "../../components/list-card/ListCard";
+import "./Home.scss";
+
+interface SongInfo {
+    name: string;
+    raga: string;
+    tala: string;
+}
+
+const songsList = [
+    {
+        name: "Maha Ganapathim",
+        raga: "Natti",
+        tala: "Adi",
+    },
+    {
+        name: "Chandra Chooda",
+        raga: "Darbari Kanada",
+        tala: "Adi",
+    },
+    {
+        name: "Pillangoviya",
+        raga: "Mohana",
+        tala: "Adi",
+    },
+    {
+        name: "Lambodhara",
+        raga: "Malahari",
+        tala: "Roopaka",
+    },
+    {
+        name: "Jayatu Jaya Vithala",
+        raga: "Mohana",
+        tala: "Adi",
+    },
+    {
+        name: "Nammamma Sharade",
+        raga: "Hamsadwani",
+        tala: "Adi",
+    },
+    {
+        name: "Ranga Nayaka",
+        raga: "Mohana",
+        tala: "Adi",
+    },
+    {
+        name: "Banallu Neene",
+        raga: "Mohana",
+        tala: "Adi",
+    },
+    {
+        name: "Bhagyada Lakshmi",
+        raga: "Madhayamavathi",
+        tala: "Adi",
+    },
+];
 
 const Home = () => {
     return (
         <div className="home">
-            <ListCard />
+            {songsList.map((songInfo) => (
+                <ListCard songInfo={songInfo} />
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
--- src/components/list-card/ListCard.tsx
+++ src/components/list-card/ListCard.tsx
@@ -1,13 +1,28 @@
-import Icon from '../FontAwesomeIcon'
 import './ListCard.scss'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-const ListCard = () => {
+interface SongInfo {
+    name: string
+    raga: string
+    tala: string
+}
+
+interface ListCardProps {
+    songInfo: SongInfo
+}
+
+const ListCard = (props: ListCardProps) => {
+    const { name, raga, tala } = props.songInfo
     return (
         <div className="list-card">
-            List
-            <Icon icon={faEnvelope} />
-        </div >
+            <div className="song-title">
+                <div className="song-name">{name}</div>
+
+            </div>
+            <div className="song-details">
+                <div className="song-ragas">ರಾಗ: {raga}</div>
+                <div className="song-tala">ತಾಳ: {tala}</div>
+            </div>
+        </div>
     )
 }
 
