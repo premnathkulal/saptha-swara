77f6528a78910323e981025c0f42d92afbcba77c
Premnathkulal <premnathkulal199@gmail.com>
2024-07-01 18:32:41 +0530

    Basec setup done for real time database

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,35 @@ import './App.scss'
 import Home from './Pages/home/Home'
 import store from './store/store'
 import AlertToast from './components/AlertToast/AlertToast'
+import { useEffect } from 'react'
+import { db } from './firebase'
+import { onValue, ref, set } from 'firebase/database'
 
 function App() {
+  useEffect(() => {
+    abc()
+  }, [])
+
+  const abc = async () => {
+    try {
+      set(ref(db, 'users' + '/1'), {
+        name: "Ganesha S"
+      });
+
+
+      ///////////////////////////////////////////////////
+      const starCountRef = ref(db, 'users/' + 2);
+      onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+      });
+      ///////////////////////////////////////////////////////////
+
+    } catch (e) {
+      console.log("Error", e);
+    }
+  }
+
   return (
     <Provider store={store}>
       <div className="app">
---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiEdWKRy5qnrJic1ly2Buf0D1A9zHYRJw",
+  authDomain: "sapta-swara.firebaseapp.com",
+  projectId: "sapta-swara",
+  storageBucket: "sapta-swara.appspot.com",
+  messagingSenderId: "1097239085790",
+  appId: "1:1097239085790:web:bf7e58da5e4011a2c7b84f",
+  measurementId: "G-DLM3YK7C6K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getDatabase();
